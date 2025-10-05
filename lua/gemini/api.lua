463027f2dd268ae2fdb947a9ce43ec80b43bed3e
mercush <barba@mit.edu>
2025-10-05 09:15:23 -0400

    starting to port to openrouter

--- lua/gemini/api.lua
+++ lua/gemini/api.lua
@@ -2,23 +2,23 @@ local uv = vim.loop or vim.uv
 
 local M = {}
 
-local API = "https://generativelanguage.googleapis.com/v1beta/models/";
+local API = "https://openrouter.ai/api/v1";
 
 M.MODELS = {
-  GEMINI_2_5_FLASH = 'gemini-2.5-flash',
-  GEMINI_2_5_FLASH_PREVIEW = 'gemini-2.5-flash-preview-04-17',
-  GEMINI_2_5_PRO_PREVIEW = 'gemini-2.5-pro-preview-03-25',
-  GEMINI_2_0_FLASH = 'gemini-2.0-flash',
-  GEMINI_2_0_FLASH_LITE = 'gemini-2.0-flash-lite',
-  GEMINI_2_0_FLASH_EXP = 'gemini-2.0-flash-exp',
-  GEMINI_2_0_FLASH_THINKING_EXP = 'gemini-2.0-flash-thinking-exp-1219',
-  GEMINI_1_5_PRO = 'gemini-1.5-pro',
-  GEMINI_1_5_FLASH = 'gemini-1.5-flash',
-  GEMINI_1_5_FLASH_8B = 'gemini-1.5-flash-8b',
+  GEMINI_2_5_FLASH = 'google/gemini-2.5-flash',
+  GEMINI_2_5_FLASH_PREVIEW = 'google/gemini-2.5-flash-preview-04-17',
+  GEMINI_2_5_PRO_PREVIEW = 'google/gemini-2.5-pro-preview-03-25',
+  GEMINI_2_0_FLASH = 'google/gemini-2.0-flash',
+  GEMINI_2_0_FLASH_LITE = 'google/gemini-2.0-flash-lite',
+  GEMINI_2_0_FLASH_EXP = 'google/gemini-2.0-flash-exp',
+  GEMINI_2_0_FLASH_THINKING_EXP = 'google/gemini-2.0-flash-thinking-exp-1219',
+  GEMINI_1_5_PRO = 'google/gemini-1.5-pro',
+  GEMINI_1_5_FLASH = 'google/gemini-1.5-flash',
+  GEMINI_1_5_FLASH_8B = 'google/gemini-1.5-flash-8b',
 }
 
 M.gemini_generate_content = function(user_text, system_text, model_name, generation_config, callback)
-  local api_key = os.getenv("GEMINI_API_KEY")
+  local api_key = os.getenv("OPENROUTER_API_KEY")
   if not api_key then
     return ''
   end
@@ -60,7 +60,7 @@ M.gemini_generate_content = function(user_text, system_text, model_name, generat
 end
 
 M.gemini_regenerate_content = function(user_text, assistant_text, system_text, model_name, generation_config, callback)
-  local api_key = os.getenv("GEMINI_API_KEY")
+  local api_key = os.getenv("OPENROUTER_API_KEY")
   if not api_key then
     return ''
   end
@@ -117,7 +117,7 @@ M.gemini_regenerate_content = function(user_text, assistant_text, system_text, m
   end
 end
 M.gemini_generate_content_stream = function(user_text, model_name, generation_config, callback)
-  local api_key = os.getenv("GEMINI_API_KEY")
+  local api_key = os.getenv("OPENROUTER_API_KEY")
   if not api_key then
     return
   end
